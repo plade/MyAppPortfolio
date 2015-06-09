9612bdcf78f791c283a71bbdded31b330dad1970
Pratik Lade <pratikla@buffalo.edu>
2015-06-09 00:27:35 -0400

    Make Project 0 ready for submission
    Create layout and make buttons show toasts

---
+++ app/src/main/java/edu/buffalo/pratikla/myappportfolio/MainActivity.java
@@ -0,0 +1,71 @@
+package edu.buffalo.pratikla.myappportfolio;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void startSpotifyStreamer(View view) {
+        makeToast("Spotify Streamer");
+    }
+
+    public void startScoresApp(View view) {
+        makeToast("Scores App");
+    }
+
+    public void startLibraryApp(View view) {
+        makeToast("Library App");
+    }
+
+    public void startBuildItBigger(View view) {
+        makeToast("Build It Bigger");
+    }
+
+    public void startXyzReader(View view) {
+        makeToast("XYZ Reader");
+    }
+
+    public void startCapstone(View view) {
+        makeToast("my Capstone App");
+    }
+
+    public void makeToast(String appName) {
+        String message = "This button will launch " + appName + "!";
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+
+}
